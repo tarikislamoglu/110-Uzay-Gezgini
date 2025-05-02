8ced7042472c3a2e42b365fd67daf7b9cbbad871
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-05-02 10:17:39 +0300

    finalupdate

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ScoreBoard from "./components/ScoreBoard";
 import "./styles.css";
 import { AiOutlineSound } from "react-icons/ai";
 import { IoVolumeMuteOutline } from "react-icons/io5";
+import { CiSettings } from "react-icons/ci";
 export default function App() {
   const STARTING_SCORE = 0;
 
@@ -14,6 +15,10 @@ export default function App() {
   const [timerRunning, setTimerRunning] = useState(false);
   const [timeLeft, setTimeLeft] = useState(gameTime);
   const [score, setScore] = useState(STARTING_SCORE);
+  const [bestScore, setBestScore] = useState(() => {
+    const stored = localStorage.getItem("bestScore");
+    return stored ? Number(stored) : 0;
+  });
 
   const [musicSound, setMusicSound] = useState(50);
   const [gameSound, setGameSound] = useState(50);
@@ -36,7 +41,12 @@ export default function App() {
   const [startClickCount, setStartClickCount] = useState(0);
   const [startCount, setStartCount] = useState(3);
   const [gameStarted, setGameStarted] = useState(false);
+
   const [inputValue, setInputValue] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const [optIsOpen, setOptIsOpen] = useState(false);
+
   useEffect(() => {
     let clearIntervalCount;
     if (timerRunning && startCount > 0) {
@@ -51,6 +61,7 @@ export default function App() {
         });
       }, 1000);
     }
+    setBestScore((prev) => (score > prev ? score : prev));
     return () => clearInterval(clearIntervalCount);
   }, [timerRunning]);
 
@@ -59,6 +70,7 @@ export default function App() {
     if (gameStarted) {
       playClick();
       playSong();
+      setUserName(() => inputValue || "Anonim");
 
       intervalId = setInterval(() => {
         setTimeLeft((prev) => {
@@ -74,6 +86,9 @@ export default function App() {
     return () => clearInterval(intervalId);
   }, [gameStarted]);
 
+  useEffect(() => {
+    localStorage.setItem("bestScore", bestScore);
+  }, [bestScore]);
   return (
     <div>
       <ScoreBoard data={{ score, timeLeft }} />
@@ -94,98 +109,113 @@ export default function App() {
         </p>
       )}
       {!timerRunning && startClickCount > 0 && (
-        <p className="text-blue-900 p-2 bg-blue-300 mx-auto text-center font-bold w-1/2 my-4 rounded-md">
-          Oyun Bitti
-        </p>
+        <div className="text-blue-900 p-2 bg-blue-300 mx-auto text-center font-bold w-1/2 my-4 rounded-md">
+          <p>Oyun Bitti</p>
+          <p>{`${userName}  adlı oyuncunun skoru : ${score}`}</p>
+          <p>best score:{bestScore}</p>
+        </div>
       )}
 
-      <div className="bg-blue-200 p-4 my-4 rounded-md flex flex-col items-center space-y-5">
-        <div className="flex space-x-5">
-          <div className="flex flex-col items-center">
-            <label htmlFor="">Music </label>
-            <div className="flex">
-              <span
-                className="cursor-pointer"
-                onClick={() => {
-                  if (musicSound > 0) {
-                    setLastMusicSound(musicSound);
-                    setMusicSound(0);
-                  } else {
-                    setMusicSound(lastMusicSound);
-                  }
-                }}
-              >
-                {musicSound > 0 ? <AiOutlineSound /> : <IoVolumeMuteOutline />}
-              </span>
-              <input
-                type="range"
-                min={0}
-                max={100}
-                onChange={(e) => setMusicSound(e.target.value)}
-                value={musicSound}
-              />
-            </div>
-          </div>
-          <div className="flex flex-col items-center">
-            <label htmlFor="">Sound </label>
-            <div className="flex">
-              <span
-                className="cursor-pointer"
-                onClick={() => {
-                  if (gameSound > 0) {
-                    setLastGameSound(gameSound);
-                    setGameSound(0);
-                  } else {
-                    setGameSound(lastGameSound);
-                  }
-                }}
-              >
-                {gameSound > 0 ? <AiOutlineSound /> : <IoVolumeMuteOutline />}
-              </span>
-              <input
-                type="range"
-                min={0}
-                max={100}
-                onChange={(e) => setGameSound(e.target.value)}
-                value={gameSound}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <label>Kullanıcı Adı:</label>
-          <input
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-          />
-          <label>Game time:</label>
-          <input
-            type="number"
-            max={300}
-            min={30}
-            onChange={(e) => setGameTime(e.target.value)}
-            value={gameTime}
-          />
-          <label>Zorluk: </label>
-          <select
-            value={difficulty}
-            onChange={(e) => setDifficulty(e.target.value)}
-          >
-            <option value="easy">Kolay</option>
-            <option value="medium">Orta</option>
-            <option value="hard">Zor</option>
-          </select>
-        </div>
+      <div className="bg-blue-200 p-4 my-4 rounded-md flex  items-baseline justify-center space-x-5 w-1/2 mx-auto">
+        <label>UserName:</label>
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button onClick={() => setOptIsOpen((prev) => !prev)}>
+          <CiSettings className="font-bold text-xl" />
+        </button>
       </div>
-
+      {optIsOpen && (
+        <div className="bg-blue-200 py-10 my-4 rounded-md flex flex-col items-center space-y-5 w-1/2 mx-auto">
+          <h2>Options</h2>
+          <div className="flex flex-col space-y-5">
+            <div className="flex justify-between items-center">
+              <label htmlFor="">Music </label>
+              <div className="flex">
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    if (musicSound > 0) {
+                      setLastMusicSound(musicSound);
+                      setMusicSound(0);
+                    } else {
+                      setMusicSound(lastMusicSound);
+                    }
+                  }}
+                >
+                  {musicSound > 0 ? (
+                    <AiOutlineSound />
+                  ) : (
+                    <IoVolumeMuteOutline />
+                  )}
+                </span>
+                <input
+                  type="range"
+                  min={0}
+                  max={100}
+                  onChange={(e) => setMusicSound(e.target.value)}
+                  value={musicSound}
+                />
+              </div>
+            </div>
+            <div className="flex  justify-between  items-center">
+              <label htmlFor="">Sound </label>
+              <div className="flex">
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    if (gameSound > 0) {
+                      setLastGameSound(gameSound);
+                      setGameSound(0);
+                    } else {
+                      setGameSound(lastGameSound);
+                    }
+                  }}
+                >
+                  {gameSound > 0 ? <AiOutlineSound /> : <IoVolumeMuteOutline />}
+                </span>
+                <input
+                  type="range"
+                  min={0}
+                  max={100}
+                  onChange={(e) => setGameSound(e.target.value)}
+                  value={gameSound}
+                />
+              </div>
+            </div>
+            <div className="flex  justify-between  items-center">
+              <label>Game time:</label>
+              <input
+                type="number"
+                max={300}
+                min={30}
+                onChange={(e) => setGameTime(e.target.value)}
+                value={gameTime}
+              />
+            </div>
+            <div className="flex  justify-between  items-center">
+              <label>Zorluk: </label>
+              <select
+                value={difficulty}
+                onChange={(e) => setDifficulty(e.target.value)}
+              >
+                <option value="easy">Kolay</option>
+                <option value="medium">Orta</option>
+                <option value="hard">Zor</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      )}
       <button
         className={`play-button ${timerRunning ? "fade-out" : "fade-in"}`}
         onClick={() => {
           setStartClickCount((prev) => prev + 1);
           setStartCount(3);
           setTimeLeft(gameTime);
+
           setScore(STARTING_SCORE);
           setTimerRunning(true);
           setGameStarted(false);
